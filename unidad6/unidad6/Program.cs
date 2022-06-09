3f573e1725fd37cc31cffe04f18dc03e5b9e9963
BelenPerezVilla <L21212579@tectijuana.edu.mx>
2022-06-09 11:38:31 -0700

    prueba 3

--- unidad6/unidad6/Program.cs
+++ unidad6/unidad6/Program.cs
@@ -34,30 +34,77 @@ namespace unidad6
     {
         static void Main(string[] args)
         {
+            string nombre, descripcion;
+            float precio;
+            int cantidad, opc;
 
-            StreamWriter sw = new StreamWriter("Productos.txt", true);
-            string Nombre, Descripcion;
-            float Precio;
-            int Stock;
-            Console.WriteLine("-----Captura de datos del producto------");
-            Console.Write("Ingrese el nombre del procto: ");
-            Nombre = Console.ReadLine();
-            Console.Write("ingrese la descripcion del procto: ");
-            Descripcion = Console.ReadLine();
-            Console.Write("Ingrese el precio del producto: ");
-            Precio = float.Parse(Console.ReadLine());
-            Console.Write("Ingrese la cantidad de stock: ");
-            Stock = int.Parse(Console.ReadLine());
-            Console.Clear();
+            do
+            {
+                Console.Write("\nMenú de Opciones\n\n1) Registrar Producto\n2) Consultar Productos\n3) Salir\n\nElija la opción: ");
+                opc = int.Parse(Console.ReadLine());
+                Console.Clear();
 
-            Amazon amazon = new Amazon(Nombre, Descripcion, Precio, Stock);
-            amazon.InventarioAmazon();
-            sw.WriteLine("Nombre del procuto: " + Nombre);
-            sw.WriteLine("Descripcion del producto: " + Descripcion);
-            sw.WriteLine("El precio del producto es: " + Precio);
-            sw.WriteLine("La cantidad del stock es: " + Stock);
+                switch (opc)
+                {
+                    case 1:
+                        StreamWriter sw = new StreamWriter("inventario.txt", true);
+                        Console.WriteLine("----- Captura de Datos -----");
+                        Console.Write("\nNombre: ");
+                        nombre = Console.ReadLine();
+                        Console.Write("Descripción: ");
+                        descripcion = Console.ReadLine();
+                        Console.Write("Precio: ");
+                        precio = float.Parse(Console.ReadLine());
+                        Console.Write("Cantidad: ");
+                        cantidad = int.Parse(Console.ReadLine());
+                        Console.Clear();
 
+                        Amazon inv = new Amazon(nombre, descripcion, precio, cantidad);
 
+                        sw.WriteLine("Nombre Producto: " + nombre + "\nDescripción: " + descripcion + "\nPrecio: $" + precio + "\nCantidad en Inventario: " + cantidad + "\n");
+                        Console.WriteLine("Producto registrado exitosamente!!\n");
+                        inv.InventarioAmazon();
+                        Console.WriteLine("\nPresione ENTER para continuar...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        sw.Close();
+                        break;
+                    case 2:
+                        try
+                        {
+                            StreamReader sr = new StreamReader("inventario.txt");
+                            string line = "";
+                            Console.WriteLine("Productos Registrados\n");
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                Console.WriteLine(line);
+                            }
+                            sr.Close();
+                            Console.WriteLine("Presione ENTER para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        catch(IOException e)
+                        {
+                            Console.WriteLine("Error, Archivo no encontrado\nDebe Capturar un producto primero");
+                            Console.WriteLine(e.Message);
+                            Console.WriteLine("Presione ENTER para continuar...");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Presione ENTER para salir del programa...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.WriteLine("Error, elija una opción correcta!\nPresione ENTER para continuar...");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                }
+            } while (opc != 3);
         }
     }
 }
